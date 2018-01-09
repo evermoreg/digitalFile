ba4acb3654515c5beb5fcdef3b12774288455a3d
tatmush <tatalmondmush@gmail.com>
2018-01-09 16:00:17 +0200

    working login page with cleanup required

---
+++ config.py
@@ -0,0 +1,3 @@
+DEBUG = True
+SQLALCHEMY_DATABASE_URI = 'sqlite://///home/tatmush/Documents/projects/flaskStructure/loginOld/building_user_login_system/finish/users.db'
+SECRET_KEY = 'Thisisasecret!'
