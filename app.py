ea6b04d9906b5115b92ab3f8ea0abdb3761704d4
tatmush <tatalmondmush@gmail.com>
2018-01-19 13:27:45 +0200

    Clean up is done. Database Migrations need handling

--- app.py
+++ app.py
@@ -1,21 +1,16 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
-#from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
-#from models import User
+from config import Config
 
 app = Flask(__name__)
-app.config.from_pyfile('config.py')
-bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
-
-#@login_manager.user_loader
-#def load_user(user_id):
-#    return User.query.get(int(user_id))
+app.config.from_object(Config)
+bootstrap = Bootstrap(app)
 
 from views import *
 
