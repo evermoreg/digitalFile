4c25deee9fb70d9735b451afc50b73409814c40b
tatmush <tatalmondmush@gmail.com>
2018-02-19 11:10:08 +0200

    skeletal structure

--- forms.py
+++ forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField
+from flask_wtf.file import FileField
 from wtforms.validators import InputRequired, Email, Length
+from werkzeug.utils import secure_filename
 
 class LoginForm(FlaskForm):
     email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
@@ -10,3 +12,8 @@ class LoginForm(FlaskForm):
 class RegisterForm(FlaskForm):
     email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
     password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+
+class messageForm(FlaskForm):
+	 receiver=StringField('to', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
+	 message=StringField('message')
+	 file=FileField()
--- models.py
+++ models.py
@@ -1,16 +1,30 @@
 from app import app, db
 from flask_login import UserMixin
+from datetime import datetime
+from io import BytesIO
+import os
 
 class User(UserMixin, db.Model):
- #id = db.Column(db.Integer, primary_key=True)
-    #username = db.Column(db.String(15), unique=True)
     email=db.Column(db.String(50), primary_key=True)
     password=db.Column(db.String(80))
-    publicKey=db.Column(db.String(500))
-    phoneNumber=db.Column(db.String(50))
+    #publicKey=db.Column(db.String(500))
+    #phoneNumber=db.Column(db.String(50))
+    messageSent=db.relationship('Messages', backref='author', lazy='dynamic')
 
     def get_id(self):
     	return str(self.email)
 
     def __repr__(self):
     	return '<User {}>'.format(self.email)
+
+class Messages(db.Model):
+	id=db.Column(db.Integer, primary_key=True)
+	receiver=db.Column(db.String(50))
+	file=db.Column(db.LargeBinary)
+	message=db.Column(db.String(500))
+	timestamp=db.Column(db.DateTime, index=True, default=datetime.utcnow)
+	sender=db.Column(db.String(50), db.ForeignKey('user.email'))
+
+	def __repr__(self):
+		return '<Message {} {}>'.format(self.message, self.file)
+
